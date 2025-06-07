74471e0a243a46a397e06eb1a4c08cc355ba5a4c
kresch159 <evgplot@tandex.ru>
2025-06-07 18:34:52 +0500

    домашняя работа - 3

---
+++ js/hw3/hw-3_3.js
@@ -0,0 +1,11 @@
+let d;
+let e;
+
+d = Number(prompt('запишите число №1'));
+e = Number(prompt('запишите число №2'));
+
+if (d > 100 || e > 100) {
+    alert('«Верно!!!»');
+} else {
+    alert('«Неверно»');
+}
